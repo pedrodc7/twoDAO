94fd8f55d56f639ccdf7b813221a0fc349293ae4
Pedro da Cunha <pedrodacunha@Pedros-MacBook-Pro.local>
2022-06-01 17:38:41 +0100

    after: Deploy your NFT bundle

---
+++ scripts/2-deploy-drop.js
@@ -0,0 +1,21 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk";
+import { readFileSync } from "fs";
+
+(async () => {
+    try {
+        const editionDropAddress = await sdk.deployer.deployEditionDrop({
+            name: "twoDAO Membership",
+            description: "An experimental DAO for a particular kind of fund",
+            image: readFileSync("scripts/assets/naruto.png"),
+            primary_sale_recipient: AddressZero,
+        });
+
+        const editionDrop = sdk.getEditionDrop(editionDropAddress);
+        const metadata = await editionDrop.metadata.get();
+        console.log("✅ Successfully deployed editionDrop contract address:", editionDropAddress);
+        console.log("✅ editionDrop metadata", metadata);
+    } catch (error) {
+        console.log("failed to deploy editionDrop contract", error);
+    }
+}) ();
